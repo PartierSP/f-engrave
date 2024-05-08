1c224be19b8c2ae715a88a0e1bfdb0ea3e833dd2
PartierSP <mikevl1973@gmail.com>
2024-05-07 23:12:35 -0400

    Added line numbers to G-Code output

--- gcode.py
+++ gcode.py
@@ -49,7 +49,7 @@ class Gcode(list):
             assert plane in (Plane.xy, Plane.xz, Plane.yz)
             if plane != self.plane:
                 self.plane = plane
-                self.write("G%d" % plane)
+                self.write('N0 '+"G%d" % plane)
 
     # If any 'cut' moves are stored up, send them to the simplification
     # algorithm and actually output them.
@@ -147,7 +147,7 @@ class Gcode(list):
                            Jstring, fstring])
 
         if cmd:
-            self.write(cmd)
+            self.write('N0 '+cmd)
 
     def set_feed(self, feed, write_it=False):
         # Set the feed rate to the given value
@@ -157,7 +157,7 @@ class Gcode(list):
         self.feed = FORMAT % self.feed_val
         self.lastf = None
         if write_it:
-            self.write(self.feed)
+            self.write('N0 '+self.feed)
             self.lastf = self.feed
 
     def set_z_feed(self, z_feed):
@@ -209,7 +209,7 @@ class Gcode(list):
     def _append_pre_post_amble(self, commands, comment):
         self.append_comment(comment)
         for line in commands.split('|'):
-            self.write(line)
+            self.write('N0 '+line)
         self.append_comment("End %s" % (comment))
 
     def append_preamble(self, commands):
@@ -220,15 +220,15 @@ class Gcode(list):
 
     def append_mode(self):
         # G90        ; Sets absolute distance mode
-        self.write('G90 (absolute)')
+        self.write('N0 '+'G90 (absolute)')
         # G91.1      ; Sets Incremental Distance Mode for I, J & K arc offsets.
         if (self.arc_fit == "center"):
-            self.write('G91.1')
+            self.write('N0 '+'G91.1')
 
     def append_units(self):
         if self.metric:
             # G21 ; sets units to mm
-            self.write('G21 (mm)')
+            self.write('N0 '+'G21 (mm)')
         else:
             # G20 ; sets units to inches
-            self.write('G20 (in)')
+            self.write('N0 '+'G20 (in)')
